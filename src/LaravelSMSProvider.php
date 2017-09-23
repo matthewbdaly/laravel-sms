48237fb709acdbfc8d88719cb7241dc5efd17bb2
Matthew Daly <matthewbdaly@gmail.com>
2017-09-23 18:11:56 +0100

    Should now expose config

--- src/LaravelSMSProvider.php
+++ src/LaravelSMSProvider.php
@@ -19,52 +19,54 @@ class LaravelSMSProvider extends ServiceProvider
 	 * @return void
 	 */
 	public function boot()
-	{
-		//
-	}
+    {
+        $this->publishes([
+            __DIR__.'config.php' => config_path('sms.php'),
+        ]);
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton('sms', function ($app) {
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('sms', function ($app) {
             $config = $app['config']->get('sms');
             switch ($config['driver']) {
-                case 'null':
-                    $driver = new NullDriver(
-                        new GuzzleClient,
-                        new GuzzleResponse
-                    );
-                    break;
-                case 'nexmo':
-                    $driver = new Nexmo(
-                        new GuzzleClient,
-                        new GuzzleResponse,
-                        [
-                            'api_key' => $config['api_key'],
-                            'api_secret' => $config['api_secret'],
-                        ]
-                    );
-                    break;
-                case 'clockwork':
-                    $driver = new Clockwork(
-                        new GuzzleClient,
-                        new GuzzleResponse,
-                        [
-                            'api_key' => $config['api_key'],
-                        ]
-                    );
-                    break;
-                default:
-                    $driver = new LogDriver(
-                        $app['log']
-                    );
-                    break;
+            case 'null':
+                $driver = new NullDriver(
+                    new GuzzleClient,
+                    new GuzzleResponse
+                );
+                break;
+            case 'nexmo':
+                $driver = new Nexmo(
+                    new GuzzleClient,
+                    new GuzzleResponse,
+                    [
+                        'api_key' => $config['api_key'],
+                        'api_secret' => $config['api_secret'],
+                    ]
+                );
+                break;
+            case 'clockwork':
+                $driver = new Clockwork(
+                    new GuzzleClient,
+                    new GuzzleResponse,
+                    [
+                        'api_key' => $config['api_key'],
+                    ]
+                );
+                break;
+            default:
+                $driver = new LogDriver(
+                    $app['log']
+                );
+                break;
             }
             return new Client($driver);
-		});
-	}
+        });
+    }
 }
