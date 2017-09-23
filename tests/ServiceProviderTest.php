08e08c57ef22b5d902ecad72bdf5057aee601519
Matthew Daly <matthewbdaly@gmail.com>
2017-09-23 17:37:50 +0100

    Failing test in place for the null driver

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -7,5 +7,7 @@ class ServiceProviderTest extends TestCase
     public function testNullDriverSetup()
     {
         $this->app['config']->set('sms.driver', 'null');
+        $client = $this->app->make('sms');
+        $this->assertInstanceOf('Matthewbdaly\SMS\Client', $client);
     }
 }
