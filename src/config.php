2fcd505d00b500c58ea8013b6fd4d9f258843251
Matthew Daly <matthewbdaly@gmail.com>
2017-09-23 18:10:30 +0100

    Added config

---
+++ src/config.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default SMS driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default SMS driver user.
+    |
+    | Supported: "log", "null", "nexmo", "clockwork"
+    |
+    */
+
+    'default' => env('SMS_DRIVER', 'log'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Drivers
+    |--------------------------------------------------------------------------
+    |
+    | Here you can define the settings for each driver. 
+    |
+    */
+
+    'drivers' => [
+
+        'clockwork' => [
+            'driver' => 'clockwork',
+            'api_key' => env('CLOCKWORK_API_KEY', null),
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'nexmo' => [
+            'driver' => 'nexmo',
+            'api_key' => env('NEXMO_API_KEY', null),
+            'api_secret' => env('NEXMO_API_secret', null),
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+    ],
+];
